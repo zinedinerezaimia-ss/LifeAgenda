55e2053d05dfd4429becb2ba44fe3a1550e73154
zinedinerezaimia-ss <zinedinerezaimia@gmail.com>
2026-02-27 13:26:50 +0100

    feat: adhan avec téléchargement et notification

--- LifeAgenda/LifeAgendaApp.swift
+++ LifeAgenda/LifeAgendaApp.swift
@@ -3,12 +3,46 @@ import SwiftUI
 @main
 struct LifeAgendaApp: App {
     @StateObject private var store = AppStore()
-    
+    @StateObject private var subscriptionService = SubscriptionService.shared
+    @StateObject private var locationService = LocationService.shared
+    @StateObject private var photoService = PhotoService.shared
+    @StateObject private var adhanService = AdhanService.shared
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environmentObject(store)
-                .preferredColorScheme(.dark)
+            Group {
+                if store.hasCompletedOnboarding {
+                    ContentView()
+                } else {
+                    OnboardingView()
+                }
+            }
+            .environmentObject(store)
+            .environmentObject(subscriptionService)
+            .environmentObject(locationService)
+            .environmentObject(photoService)
+            .environmentObject(adhanService)
+            .preferredColorScheme(.dark)
+            .onAppear {
+                if store.hasCompletedOnboarding {
+                    locationService.fetchLocation()
+                    store.checkYesterdayPunishments()
+                    // Télécharge l'adhan si activé
+                    if adhanService.adhanEnabled {
+                        Task { await adhanService.downloadAdhanIfNeeded() }
+                    }
+                }
+            }
+            .onChange(of: locationService.coordinate) { _, coord in
+                if coord != nil {
+                    store.refreshPrayerTimes()
+                    store.syncToWidget()
+                    // Replanifie l'adhan avec les nouvelles heures
+                    if adhanService.adhanEnabled {
+                        adhanService.scheduleAllAdhan(prayerTimes: store.prayerTimes)
+                    }
+                }
+            }
         }
     }
 }
